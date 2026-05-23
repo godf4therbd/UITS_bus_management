fab397de92252c2b0157714ed9339b98841e1f10
Biswajit Roy <parag.parag110@gmail.com>
2026-05-23 13:43:01 +0600

    Deploy to GitHub Pages

--- src/constants/notifications.ts
+++
@@ -1,9 +0,0 @@
-export const PREBUILT_MESSAGES = [
-  'Bus is full',
-  'Bus is delayed by 10 minutes',
-  'Bus is delayed by 15 minutes',
-  'Bus is delayed by 20 minutes',
-  'Bus will depart shortly',
-  'Bus has departed',
-  'Traffic on the route - expect delays',
-];
